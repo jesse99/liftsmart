1a68d3fb8197627a462b7a2ba2e00095828d66de
Jesse Vorisek <jesse.vorisek@gmail.com>
2020-06-06 11:27:30 -0700

    Pop exercise view when the user finishes

--- liftsmart/Views/ExerciseDurationsView.swift
+++ liftsmart/Views/ExerciseDurationsView.swift
@@ -2,12 +2,14 @@
 //  Copyright © 2020 MushinApps. All rights reserved.
 import SwiftUI
 
-struct ExerciseDurationsView: View { // TODO: rename this ExerciseDurationsView
+struct ExerciseDurationsView: View {
     var exercise: Exercise
     let durations: [DurationSet]
     let targetDuration: Int?
     @State var start_modal: Bool = false
     @State var timer_modal: Bool = false
+    @State var setIndex: Int = 0
+    @Environment(\.presentationMode) private var presentation
     
     var body: some View {
         VStack {
@@ -19,7 +21,7 @@ struct ExerciseDurationsView: View { // TODO: rename this ExerciseDurationsView
                 Text(subTitle()).font(.headline)        // 60s
                 Spacer()
 
-                Button("Start", action: onStart)
+                Button(startLabel(), action: onStart)
                     .font(.system(size: 40.0))
                     .sheet(isPresented: self.$start_modal, onDismiss: self.onStartCompleted) {TimerView(duration: self.duration())}
                 Spacer().frame(height: 50)
@@ -51,14 +53,18 @@ struct ExerciseDurationsView: View { // TODO: rename this ExerciseDurationsView
     func onStart() {
         // TODO:
         // update current (can do this with an onDismiss: callback, probably need another show_timer var)
-        // update title and subtitle views (probably need to have them use a @State var)
-        // when run out of sets Start buttion needs to change to Done
-        // when Done is pressed need to return to WorkoutView
-        self.start_modal = true
+        // curent should also be updated if user hits back button mid-set
+        // when come back setIndex should be set to current value
+        if setIndex < durations.count {
+            self.start_modal = true
+        } else {
+            // Pop this view. Note that currently this only works with a real device,
+            self.presentation.wrappedValue.dismiss()
+        }
     }
     
     func onStartCompleted() {
-        
+        self.setIndex += 1
     }
     
     func onStartTimer() {
@@ -66,37 +72,46 @@ struct ExerciseDurationsView: View { // TODO: rename this ExerciseDurationsView
     }
     
     func duration() -> Int {
-        var setIndex = 0
-        if let current = exercise.current {
-            setIndex = current.setIndex
+        if setIndex < durations.count {
+            return durations[setIndex].secs
+        } else {
+            return durations.last!.secs
         }
-        
-        return durations[setIndex].secs
     }
     
     func title() -> String {
-        var setIndex = 0
-        if let current = exercise.current {
-            setIndex = current.setIndex
+        if setIndex < durations.count {
+            return "Set \(setIndex+1) of \(durations.count)"
+        } else if durations.count == 1 {
+            return "Finished"
+        } else {
+            return "Finished all \(durations.count) sets"
         }
-        
-        return "Set \(setIndex+1) of \(durations.count)"
     }
 
     func subTitle() -> String {
-        var setIndex = 0
-        if let current = exercise.current {
-            setIndex = current.setIndex
+        if setIndex >= durations.count {
+            return ""
         }
-        
-        if let target = targetDuration {
-            return "\(durations[setIndex]) (target is \(target)x)"
 
+        let duration = durations.last!
+        if let target = targetDuration {
+            return "\(duration) (target is \(target)x)"
         } else {
-            return "\(durations[setIndex])"
+            return "\(duration)"
         }
     }
     
+    func startLabel() -> String {
+        if setIndex == 0 {
+            return "Start"
+        } else if (setIndex == durations.count) {
+            return "Done"
+        } else {
+            return "Next"
+        }
+    }
+
     func weight(_ reps: RepsSet) -> String {
         if let current = exercise.current {
             return "\(Int(current.weight * reps.percent)) lbs"  // TODO: need to use apparatus
@@ -108,7 +123,7 @@ struct ExerciseDurationsView: View { // TODO: rename this ExerciseDurationsView
 }
 
 struct ExerciseView_Previews: PreviewProvider {
-    static let durations = [DurationSet(secs: 60, restSecs: 60)!]
+    static let durations = [DurationSet(secs: 60, restSecs: 60)!, DurationSet(secs: 30, restSecs: 60)!, DurationSet(secs: 15, restSecs: 60)!]
     static let sets = Sets.durations(durations)
     static let modality = Modality(Apparatus.bodyWeight, sets)
     static let exercise = Exercise("Burpees", "Burpees", modality)
