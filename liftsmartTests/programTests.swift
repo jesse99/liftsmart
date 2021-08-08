5f80c77d4a7fc3bb4e7e2f06e8e7a2652b519fea
Jesse Vorisek <jesse.vorisek@gmail.com>
2021-08-08 15:52:00 -0700

    renaming

--- liftsmartTests/programTests.swift
+++ liftsmartTests/programTests.swift
@@ -4,7 +4,7 @@ import SwiftUI
 import XCTest
 @testable import liftsmart
 
-class liftsmartTests: XCTestCase {
+class programTests: XCTestCase {
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
